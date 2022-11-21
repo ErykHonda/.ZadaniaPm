b17f4dc80d383ff894f9e2d48420618d9cc01d1a
ErykHonda <eryk.minecraft@outlook.com>
2022-11-21 11:06:06 +0100

    lekcja 21.11.2022
    update po 2 lekcji

--- Ziemia/Ziemia2.0/script.js
+++ Ziemia/Ziemia2.0/script.js
@@ -7,11 +7,11 @@ function wywolaj() {
     //console.log(x);  
 
 }
-
+/*
 var a = 'string tekst a ';
 var b = "string tekst b";
 var c = "문자열 텍스트 c  文字列テキスト c  текст строки с";
-var d = a+b;
+var d = a+a;
 document.write(a);
 document.write("<br>");
 document.write(b);
@@ -19,3 +19,59 @@ document.write("<br>");
 document.write(c);
 document.write("<br>");
 document.write(d);
+
+let x = String("sadsa");
+let y = new String('lorem')
+let xy= x+y+56+a
+document.write("<br>");
+document.write(x);
+document.write("<br>");
+document.write(y);
+document.write("<br>");
+document.write(xy);
+
+const zx = 'kot'.charAt(1);
+const zxz = 'pies'[1];
+console.log(zxz)
+
+const tekst = 'kot';
+//tekst.charAt(1)='a';//nie działa
+tekst[1]='a'
+console.log(tekst);
+document.write(tekst);
+
+
+i1 = 56+2;
+i2 = '57'+1 ;
+console.log(i1)
+console.log(i2)
+if(i1<i2)
+{
+    document.write("i1 < i2");
+}else if(i1>i2)
+{
+    document.write("i1 > i2");
+}else 
+{
+    document.write("równe");
+}
+
+
+
+var s_prim = "문자열 텍스트 c  文字列テキスト c  текст строки с";
+var s_prim1 = "문자열 텍스트 c  文字列テキスト c  текст строки с";
+var pr2 = s_prim+s_prim1;
+let s_obj =  new String(s_prim)
+var obj2 = s_obj+ s_obj;
+document.write(typeof pr2)
+document.write("<br>")
+document.write(typeof obj2)
+
+var y = 'pies'[1];
+document.write(typeof y)
+
+*/
+
+var cos = 'kot';
+cos = String(cos);
+console.log(typeof cos)
