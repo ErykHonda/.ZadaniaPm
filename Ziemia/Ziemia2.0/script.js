7d6765824664ec4cfebc71904b1b934dd8fd1c83
ErykHonda <eryk.minecraft@outlook.com>
2022-11-21 10:11:44 +0100

    lekcja 21.11.2022
    update po 1 lekcji

---
+++ Ziemia/Ziemia2.0/script.js
@@ -0,0 +1,21 @@
+function wywolaj() {
+
+    const x = document.getElementById("pierwszy")
+    x.innerHTML = "po łacomoe"
+
+    //const y = x.innerHTML
+    //console.log(x);  
+
+}
+
+var a = 'string tekst a ';
+var b = "string tekst b";
+var c = "문자열 텍스트 c  文字列テキスト c  текст строки с";
+var d = a+b;
+document.write(a);
+document.write("<br>");
+document.write(b);
+document.write("<br>");
+document.write(c);
+document.write("<br>");
+document.write(d);
