374d62f7f08c475dcde420aa501476c337950d13
ErykHonda <eryk.minecraft@outlook.com>
2022-12-24 00:12:00 +0100

    Update
    dodane dynamiczne języki i style
    -działające listy
    -działające dodawanie do list

--- Milkyway/Milkyway1.1/script.js
+++ Milkyway/Milkyway1.1/script.js
@@ -6,24 +6,73 @@ const ListyIn = [ListaIn1];
 const ListyOut = [ListaOut1];
 //tłumaczenie
 var ListNumber,buttValue,NonValueAdd,ShopList,DeleteList,NewList;//języki
-function StartingSeting(Lang)
+function StartingSeting(Lang,Theme)
 {
+    setTheme(Theme)
     AddMissingElement()
     setLanguage(Lang)
+    
+    if(document.getElementsByClassName("list").length>=1)
+    {
+        HideElement(0)
+    }
+
+}
+
+function setTheme(color)
+{//<link rel="stylesheet" href="Theme/pink-theme.css"></link>
+    var theme = '<link rel="stylesheet" href="'
+    switch(color)
+    {
+        case "PINK":
+            theme+="Style/pink-theme.css";
+            break;
+        case "GREEN":
+            theme+="Style/green-theme.css";
+            break;
+        default:
+            theme+="Style/pink-theme.css";
+            break;
+    }
+    theme+='"></link>'
+    document.getElementById("LoadTheme").innerHTML=theme;
+}   
+
+function validation(n)
+{
+    var zawartosc = document.getElementsByClassName("get-text")[n].value
+    if(zawartosc!="")
+    {
+        document.getElementsByClassName("validation")[n].innerHTML=""
+        return true;
+    }else 
+    {
+        document.getElementsByClassName("validation")[n].innerHTML=NonValueAdd
+        return false;
+    }
+    
+}
+
+function AddToList(n)
+{
+    if(validation(n)==true)
+    {
+        ListyIn[n][ListyIn[n].length]=document.getElementsByClassName("get-text")[n].value;
+        Refresh()
+    }
 }
 
 function HideElement(who)
 {
     const lista = document.getElementsByClassName("to-hide")[who]
     const OnOff = document.getElementsByClassName("minimalized")[who].childNodes[0]
-    console.log(OnOff)
-    if(OnOff.innerHTML=="--")
+    if(OnOff.innerHTML=="-")
     {
         OnOff.innerHTML="+";
         $(lista).hide()
     }else
     {
-        OnOff.innerHTML="--";
+        OnOff.innerHTML="-";
         $(lista).show()
     }
     
@@ -53,6 +102,12 @@ function setLanguage(Lang)
             break;
         case "ENG":
             jasonLanguage= "Language/ENG-Language.json";
+            break;
+        case "FR":
+            jasonLanguage="Language/FR-Language.json"
+            break;
+        case "UK":
+            jasonLanguage="Language/UK-Language.json"
             break;
         default:
             jasonLanguage= "Language/PL-Language.json";
@@ -84,9 +139,19 @@ function setLanguage(Lang)
 
 function Refresh()
 {
-    for(var i = 0 ; i<=document.getElementsByClassName("list").length ; i++)
+    
+    for(var i = 1 ; i<=(document.getElementsByClassName("list").length) ; i++)
     {
-        LoadList(i)
+        
+        var PM = document.getElementsByClassName("minimalized")[i-1].childNodes[0]
+        if(PM.innerHTML==="+")
+        {
+            LoadList(i)
+            HideElement(i-1)
+        }else
+        {
+            LoadList(i)
+        }
     }
     LoadFutter()
 }
@@ -124,18 +189,18 @@ function LoadBufor(n)
     bufor +='<div class="list">';
     bufor +='<div class="l-top">';
     bufor +='<h1>'+ListNumber+'<span>'+(n)+'</span></h1>';
-    bufor +='<div class="minimalized">';
-    bufor +='<h1 onclick="HideElement('+(n-1)+')">--</h1>';
+    bufor +='<div class="minimalized" onclick="HideElement('+(n-1)+')">';
+    bufor +='<h1>-</h1>';
     bufor +=    '</div>';
     bufor += '</div>';
     bufor += '<div class="to-hide" style="">';
     bufor +=    '<div class="l-mid">';
     bufor +=         '<div class="mid-up">';
     bufor +=             '<div class="mid-up-in">';
-    bufor +=                '<input type="button" value="'+buttValue+'" class="button-add">';
+    bufor +=                '<input type="button" value="'+buttValue+'" class="button-add" onclick="AddToList('+(n-1)+')">';
     bufor +=            '</div>';
     bufor +=            '<div class="mid-up-in">';
-    bufor +=                 '<input type="text" class="get-text">';
+    bufor +=                 '<input type="text" class="get-text" onkeyup="validation('+(n-1)+')"> ';
     bufor +=            '</div>  ';
     bufor +=             '<div class="mid-up-in">';
     bufor +=                '<span class="validation">';
@@ -172,20 +237,18 @@ function LoadBufor(n)
 
 function AddList()
 {
-    var NewList = []
+    var NewListIn = []
+    var NewListOut = []
     if(ListyIn.length==ListyOut.length)
     {
-        ListyIn[ListyIn.length]=NewList
-        ListyOut[ListyOut.length]=NewList
-        console.log("=")
+        ListyIn[ListyIn.length]=NewListIn
+        ListyOut[ListyOut.length]=NewListOut
     }else if(ListyIn.length>=ListyOut.length)
     {
-        ListyOut[ListyOut.length]=NewList
-        console.log(">")
+        ListyOut[ListyOut.length]=NewListOut
     }else
     {
-        ListyIn[ListyIn.length]=NewList
-        console.log("<")
+        ListyIn[ListyIn.length]=NewListIn
     }
     
     
@@ -193,9 +256,9 @@ function AddList()
     var bufor = LoadBufor((document.getElementsByClassName("list").length)+1);
     var footer = body.childNodes[(body.childNodes.length)-2].outerHTML
     body.childNodes[(body.childNodes.length)-2].outerHTML=bufor
-    //console.log(footer)
     body.innerHTML+=footer
-    body.innerHTML+='<br>'
+    body.innerHTML+=' '
+    HideElement((document.getElementsByClassName("list").length-1))
 }
 
 function LoadList(nrList)
