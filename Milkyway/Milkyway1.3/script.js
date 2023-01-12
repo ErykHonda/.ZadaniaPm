964e7710776fb15a022caa69ad5f15f647090946
ErykHonda <eryk.minecraft@outlook.com>
2023-01-12 10:16:49 +0100

    up

--- Milkyway/Milkyway1.3/script.js
+++ Milkyway/Milkyway1.3/script.js
@@ -9,18 +9,26 @@ function Validate()
     console.log(MandY[1])
     const LastDay = new Date(MandY[1],MandY[0]+1,0).getDate()
     const FirstDay = new Date(MandY[1],MandY[0],1).getDay()
+    const PastLastDay = new Date(MandY[1],MandY[0],0).getDate()
     buffor = '<tr><td class="KL-Kartka">'+DaysP[0]+'</td><td class="KL-Kartka">'+DaysP[1]+'</td><td class="KL-Kartka">'+DaysP[2]+'</td><td class="KL-Kartka">'+DaysP[3]+'</td><td class="KL-Kartka">'+DaysP[4]+'</td><td class="KL-Kartka">'+DaysP[5]+'</td><td class="KL-Kartka">'+DaysP[6]+'</td></tr>';
     buffor += '<tr>'
+    document.getElementById("KL-M-Name").innerHTML = MonthsP[MandY[0]]
     var Day = 0;
-    for(var i = 0;i<FirstDay;i++)
+    for(var i = FirstDay;i>0;i--)
+    {
+        buffor+='<td class="KL-Kartka-PM">'+(PastLastDay-i+1)+'</td>'
+        Day++
+    }
+    
+    var czykonec = false
+    console.log(LastDay)
+    console.log(Day)
+    for(var i = Day ; i < 7 ; i++)
     {
         buffor+='<td class="KL-Kartka"></td>'
         Day++
     }
     buffor += '</tr>'
-    var czykonec = false
-    console.log(LastDay)
-    console.log(Day)
     while(czykonec == false)
     {
         buffor += '<tr>'
